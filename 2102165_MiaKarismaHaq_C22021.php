ed4a4c1b58fdaa3ddf4ac3c3b14e7b7e838d6a19
unknown <miakarisma87@gmail.com>
2022-03-03 16:38:00 +0700

    commit pertama

---
+++ 2102165_MiaKarismaHaq_C22021.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Tugas Praktikum1 Git/Github</title>
+	</head>
+	<body>
+		<?php
+			//Tugas Idividu membuat file berekstensi .php yang berisi nama dan NIM
+			echo "<p>Nama : Mia Karisma Haq</p>";
+			echo "<p>NIM : 2102165</p>";
+		?>
+	</body>
+</html>
