2283f3f9f29fd242db3c93712bed1cb628c8955b
unknown <aliffaturahman@upi.edu>
2022-03-05 22:58:21 +0700

    Commit by alif

---
+++ 2107377_AlifFaturahmanFirdaus_C22021.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+	<head>
+    <title>Tugas Praktikum1 Git/Github</title>
+	</head>
+	<body>
+		<?php
+			//Membuat file yang berisi nama dan NIM dengan eksitensi .php
+			echo "<p>Nama : Alif Faturahman Firdaus</p>";
+			echo "<p>NIM : 2107377</p>";
+		?>
+	</body>
+</html>
