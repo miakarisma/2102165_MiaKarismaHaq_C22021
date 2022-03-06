c0b7cc77cf301efd3b71027570fa9c63686199d3
MuhammadRizki8 <muhammadrizki@upi.edu>
2022-03-06 21:21:11 +0700

    commit by Rizki

---
+++ 2107922_MuhammadRizki.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Tugas Praktikum1 Git/Github</title>
+	</head>
+	<body>
+		<?php
+			//Tugas Idividu membuat file berekstensi .php yang berisi nama dan NIM
+			echo "<p>Nama : Muhammad Rizki</p>";
+			echo "<p>NIM  : 2107922</p>";
+			echo "<p>Kelas: C2</p>";
+		?>
+	</body>
+</html>
